8368e1ba84b08947f72606dad37a3c820d8868ae
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2025-10-07 13:13:58 +0200

    Add erase flash message

--- booster/src/display_mngr.c
+++ booster/src/display_mngr.c
@@ -70,6 +70,10 @@ void display_mngr_change_status(uint8_t status, const char *details) {
       snprintf(status_str, sizeof(status_str),
                details != NULL ? details : "Firmware ready for upgrade");
       break;
+    case 7:
+      snprintf(status_str, sizeof(status_str),
+               details != NULL ? details : " Erase flash, not reboot ");
+      break;
     default:
       snprintf(status_str, sizeof(status_str), "   Launching App...  ");
       break;
